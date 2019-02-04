9d2889b41364d4a409059e9c030d7f6c3b10438d
Marcel Montrey <MarcelMontrey@users.noreply.github.com>
2019-02-04 12:48:35 -0500

    Renamed incursion to incursions.

--- RandomWalk2D/Stats.java
+++ RandomWalk2D/Stats.java
@@ -5,13 +5,13 @@ import jmrm.FileIO;
 // This class records and saves the stats we wish to gather.
 public class Stats {
   public static String[] general; // Records general simulation data: the winner (Ns or Ms) and the incursion index.
-  private static int incursion; // Tracks the incursion index over the course of the simulation.
+  private static int incursions; // Tracks the incursion index over the course of the simulation.
   
   // Initialize our record keeping.
   public static void init() {
     general = new String[2];
     general[0] = "N\tM\tCycles\tIncursion Index";
-    incursion = 0;
+    incursions = 0;
   }
   
   // Record stats for this cycle.
@@ -21,8 +21,7 @@ public class Stats {
       for(int j = 0; j < Params.WIDTH; j++) {
         // If there's a Neanderthal band in one of these positions, add the incursion index.
         if(RandomWalk.bands[i][j].type == Band.N) {
-          //incursion += i + 1 - Params.START_DEPTH;
-          incursion++;
+          incursions++;
         }
       }
     }
@@ -37,10 +36,10 @@ public class Stats {
     general[1] += "\t" + RandomWalk.cycles;
     
     // Rescale the incursion index by the array's depth.
-    incursion /= Params.DEPTH;
+    incursions /= Params.DEPTH;
     
     // If the winner was Neanderthals, record the incursion index as 0. Otherwise, record the incursion index.
-    general[1] += (Band.N == RandomWalk.bands[0][0].type) ? "\t0" : "\t" + incursion;
+    general[1] += (Band.N == RandomWalk.bands[0][0].type) ? "\t0" : "\t" + incursions;
     
     // Get the file path for this simulation, creating a directory for it if one does not already exist.
     String path = FileIO.mkSubdir(Params.WRITE_PATH, RandomWalk.offset * Params.N_RUNS);
